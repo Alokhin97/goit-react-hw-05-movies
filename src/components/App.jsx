660b8ca22e1ad14c786812e945811ce932cfa3a3
Liubov Murarova <murarova.lubov@gmail.com>
2023-06-12 19:05:48 +0300

    next try

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -43,16 +43,16 @@ export const App = () => {
   return (
     <>
       <Routes>
-        <Route path="/" element={ <Layout /> }>
-          <Route index element={ <Home /> } />
-          <Route path="movies" element={ <Movies /> } />
+        {/* <Route path="/" element={ <Layout /> }> */}
+          <Route path="/" element={ <Home /> } />
+          {/* <Route path="movies" element={ <Movies /> } />
           <Route path="movies/:movieId" element={ <MovieDetails /> }>
             <Route path="cast" element={ <Cast /> } />
             <Route path="reviews" element={ <Reviews /> } />
           </Route>
-          <Route path="*" element={ <NotFound /> } />
-          <Route path="messages" element={<p>Hello</p>} />
-        </Route>
+          <Route path="*" element={ <NotFound /> } /> */}
+          <Route path="/messages" element={<p>Hello</p>} />
+        {/* </Route> */}
       </Routes>
     </>
   );
