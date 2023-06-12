019547cd0d1b663a21bdd58e868ff715b489c542
Liubov Murarova <murarova.lubov@gmail.com>
2023-06-12 19:12:29 +0300

    links added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,15 @@
 import { Route, Routes } from 'react-router-dom';
 
+import { NavLink } from "react-router-dom";
+
 export const App = () => {
   return (
     <>
+      <NavLink to="/">Home</NavLink>
+      <NavLink to="/messages">Messages</NavLink>
       <Routes>
-          <Route path="/" element={ <p>Home</p> } />
-          <Route path="/messages" element={<p>Hello</p>} />
+        <Route path="/" element={ <p>Home</p> } />
+        <Route path="/messages" element={ <p>Hello</p> } />
       </Routes>
     </>
   );
