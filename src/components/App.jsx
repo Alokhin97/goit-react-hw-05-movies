bc7a22ba4f1dabf3717a08440120488dd84c57e5
Liubov Murarova <murarova.lubov@gmail.com>
2023-06-12 19:22:58 +0300

    layout removed

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,65 @@
 import { Route, Routes } from 'react-router-dom';
 
-import { NavLink } from "react-router-dom";
+import { AppMenu } from './AppMenu/AppMenu.jsx';
+import { lazy } from 'react';
 
+// const Layout = lazy(() =>
+//   import('./Layout').then(module => ({
+//     ...module,
+//     default: module.Layout,
+//   }))
+// );
+
+const Movies = lazy(() =>
+  import('../pages/Movies/Movies').then(module => ({
+    ...module,
+    default: module.Movies,
+  }))
+);
+const MovieDetails = lazy(() =>
+  import('../pages/MovieDetails/MovieDetails').then(module => ({
+    ...module,
+    default: module.MovieDetails,
+  }))
+);
+const Home = lazy(() =>
+  import('../pages/Home/Home').then(module => ({
+    ...module,
+    default: module.Home,
+  }))
+);
+const Cast = lazy(() =>
+  import('./Cast/Cast').then(module => ({
+    ...module,
+    default: module.Cast,
+  }))
+);
+const Reviews = lazy(() =>
+  import('./Review/Reviews').then(module => ({
+    ...module,
+    default: module.Reviews,
+  }))
+);
+const NotFound = lazy(() =>
+  import('../pages/NotFound/NotFound').then(module => ({
+    ...module,
+    default: module.NotFound,
+  }))
+);
 export const App = () => {
   return (
     <>
-      <NavLink to="/">Home</NavLink>
-      <NavLink to="/messages">Messages</NavLink>
+     <AppMenu />
       <Routes>
-        <Route path="/" element={ <p>Home</p> } />
-        <Route path="/messages" element={ <p>Hello</p> } />
+        {/* <Route path="/" element={<Layout />}> */}
+          <Route path="/" element={<Home />} />
+          <Route path="/movies" element={<Movies />}></Route>
+          <Route path="/movies/:movieId" element={<MovieDetails />}>
+            <Route path="cast" element={<Cast />} />
+            <Route path="reviews" element={<Reviews />} />
+          </Route>
+          <Route path="*" element={<NotFound />} />
+        {/* </Route> */}
       </Routes>
     </>
   );
