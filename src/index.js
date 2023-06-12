91895bd6413cf3829e10266f72fb459e8d65b4d9
Liubov Murarova <murarova.lubov@gmail.com>
2023-06-12 21:02:05 +0300

    change NavLink to Link

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import ReactDOM from 'react-dom/client';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/goit-react-hw-05-movies/">
+    <BrowserRouter basename="/goit-react-hw-05-movies">
       <App />
     </BrowserRouter>
   </React.StrictMode>
